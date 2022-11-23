52d4d89c0100159b8abf0d1b3c0ac6ba5dd25252
Daeng Deni Mardaeni <ddeni05@gmail.com>
2022-11-23 15:29:58 +0700

    update pdf

--- resources/views/pages/klinik/examinations/pdf.blade.php
+++ resources/views/pages/klinik/examinations/pdf.blade.php
@@ -19,8 +19,12 @@
             <td style="width:70%">: {{ $examination->examination_code }}<td>
         </tr>
         <tr>
+            <td style="witdh:30%;font-weight:bold">Examination Date<td>
+            <td style="width:70%">: {{ $examination->examination_date }}<td>
+        </tr>
+        <tr>
             <td style="witdh:30%;font-weight:bold">Full Name<td>
-            <td style="width:70%">: {{ $user->name }}<td>
+            <td style="width:70%">:{{ ($user->info->title_prefix !='' ? $user->info->title_prefix.'. ' : '').$user->name.($user->info->title_suffix!='' ? ', '.$user->info->title_suffix : '') }}<td>
         </tr>
         <tr>
             <td style="witdh:30%;font-weight:bold">Doctor<td>
