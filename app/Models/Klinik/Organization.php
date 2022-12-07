c04c8c82bee1c25d8c05305628d7cb1d23c40a3f
Daeng Deni Mardaeni <ddeni05@gmail.com>
2022-12-07 08:31:27 +0700

    Add Model Ortganization

---
+++ app/Models/Klinik/Organization.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models\Klinik;
+
+use App\Core\Traits\SpatieLogsActivity;
+use App\Models\Master\City;
+use App\Models\Master\Country;
+use App\Models\Master\District;
+use App\Models\Master\Province;
+use App\Models\Master\SubDistrict;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Organization extends Model
+{
+    use SpatieLogsActivity, HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'organization_id',
+        'email',
+        'phone',
+        'fax',
+        'logo',
+        'country_id',
+        'province_id',
+        'city_id',
+        'district_id',
+        'sub_district_id',
+        'address',
+        'postal_code',
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
+
+    public function subdistrict()
+    {
+        return $this->belongsTo(SubDistrict::class);
+    }
+}
