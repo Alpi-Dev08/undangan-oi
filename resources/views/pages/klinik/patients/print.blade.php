dadfde5f5bf4a38fcec3587bb5caff51fdf48790
Daeng Deni Mardaeni <ddeni05@gmail.com>
2023-10-25 22:23:58 +0700

    update design print, penambahan looping jumlah label

--- resources/views/pages/klinik/patients/print.blade.php
+++ resources/views/pages/klinik/patients/print.blade.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+@php use Carbon\Carbon; @endphp
+    <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
@@ -9,32 +10,38 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
-<div style="font-size:9px;height:2.3cm;width:6cm;border:1px solid black;padding: 5px;padding-top:2px;">
-    <table style="width: 100%" cellpadding="0" cellspacing="0">
-        <tr>
-            <td colspan="2" style="text-align: center;border-bottom: 1px solid black; font-weight: bold">KLINIK SATRIABUDI DHARMA MEDIKA</td>
-        </tr>
-        <tr>
-            <td style="width: 35%;">Nama Dokter</td>
-            <td style="width: 65%;">: {{ $dokter }}</td>
-        </tr>
-        <tr>
-            <td>Nama</td>
-            <td>: {{ $examination->patient->user->name }}</td>
-        </tr>
-        <tr>
-            <td>Nomor MR</td>
-            <td>: {{ $examination->patient->user->mr->medical_record_code }}</td>
-        </tr>
-        <tr>
-            <td>TTL</td>
-            <td>: {{ \Carbon\Carbon::parse($examination->patient->user->info->date_of_birth)->locale('id')->translatedFormat('d F Y') }}</td>
-        </tr>
-        <tr>
-            <td>Jenis Kelamin</td>
-            <td>: {{ $examination->patient->user->info->gender->name }}</td>
-        </tr>
-    </table>
+<div class="row" style="padding:10px;">
+@for($i=0;$i<$jumlah;$i++)
+    <div style="font-size:9px;height:2.3cm;width:6cm;border:1px solid black;padding: 5px;padding-top:2px;margin-right:5px;margin-bottom: 10px">
+
+        <table style="width: 100%" cellpadding="0" cellspacing="0">
+            <tr>
+                <td colspan="2" style="text-align: center;border-bottom: 1px solid black; font-weight: bold">KLINIK SATRIABUDI DHARMA MEDIKA</td>
+            </tr>
+            <tr>
+                <td style="width: 35%;">Nama Dokter</td>
+                <td style="width: 65%;">: {{ $dokter }}</td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>: {{ $examination->patient->user->name }}</td>
+            </tr>
+            <tr>
+                <td>Nomor MR</td>
+                <td>: {{ $examination->patient->user->mr->medical_record_code }}</td>
+            </tr>
+            <tr>
+                <td>TTL</td>
+                <td>: {{ Carbon::parse($examination->patient->user->info->date_of_birth)->locale('id')->translatedFormat('d F Y') }}</td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>: {{ $examination->patient->user->info->gender->name }}</td>
+            </tr>
+        </table>
+
+    </div>
+@endfor
 </div>
 </body>
 <script>
