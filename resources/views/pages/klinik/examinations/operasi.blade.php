080d2d368d97c838c6c7734597fb787fa4e633d9
Daeng Deni Mardaeni <ddeni05@gmail.com>
2024-07-08 19:58:40 +0700

    Feature : Update Output Penandaan Titik Operasi Menjadi Lingkaran

--- resources/views/pages/klinik/examinations/operasi.blade.php
+++ resources/views/pages/klinik/examinations/operasi.blade.php
@@ -56,13 +56,12 @@
             margin-top: 3cm;
             margin-bottom: 120px;
         }
-
-
+        
         #point {
             position: absolute;
-            width: 15px;
-            height: 15px;
-            background-color: red;
+            width: 40px;
+            height: 40px;
+            border: 2px solid red;
             border-radius: 50%;
             left: {{ $data->coordinate_x ?? 0 }}px; /* Adjust the position of the point */
             top:{{ $data->coordinate_y+218 ?? 0 }}px;
