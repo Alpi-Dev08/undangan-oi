6079267ec87efeee8384e1b41ad3d7be84c23bc5
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-02-23 07:03:57 +0700

    fix(drugs): perbaiki penamaan route untuk detail dan reduceDetail
    - Mengubah nama route 'drugs.detail' menjadi 'drugs.detail_' untuk menghindari konflik.
    - Mengubah nama route 'drugs.reduceDetail' menjadi 'drugs.reduceDetail_' untuk konsistensi.
    - Memperbarui nama route 'klinik.drugs.detail' menjadi 'drugs.detail__'.
    - Memperbarui nama route 'klinik.drugs.reduceDetail' menjadi 'drugs.reduceDetail__'.
    - Menyesuaikan penamaan route updateDetail dan updateDetailReduce untuk 'klinik'.

--- routes/web.php
+++ routes/web.php
@@ -236,14 +236,14 @@
             Route::resource('units', UnitController::class);
             Route::resource('drugs', DrugsController::class);
             Route::get('drugs/{drug}/detail', [DrugsController::class, 'detail'])->name('drugs.detail');
-            Route::put('drugs/{drug}/detail', [DrugsController::class, 'updateDetail'])->name('drugs.detail');
-            Route::get('klinik/drugs/{drug}/detail', [DrugsController::class, 'showDetail'])->name('drugs.detail');
+            Route::put('drugs/{drug}/detail', [DrugsController::class, 'updateDetail'])->name('drugs.detail_');
+            Route::get('klinik/drugs/{drug}/detail', [DrugsController::class, 'showDetail'])->name('drugs.detail__');
             Route::put('klinik/drugs/{drug}/detail', [DrugsController::class, 'updateDetail'])->name('drugs.update1');
 
             Route::get('drugs/{drug}/reduceDetail', [DrugsController::class, 'reduceDetail'])->name('drugs.reduceDetail');
-            Route::put('drugs/{drug}/reduceDetail', [DrugsController::class, 'updateDetail'])->name('drugs.reduceDetail');
-            Route::get('klinik/drugs/{drug}/reduceDetail', [DrugsController::class, 'showDetailReduce'])->name('drugs.reduceDetail');
-            Route::put('klinik/drugs/{drug}/reduceDetail', [DrugsController::class, 'updateDetailReduce'])->name('drugs.update1');
+            Route::put('drugs/{drug}/reduceDetail', [DrugsController::class, 'updateDetail'])->name('drugs.reduceDetail_');
+            Route::get('klinik/drugs/{drug}/reduceDetail', [DrugsController::class, 'showDetailReduce'])->name('drugs.reduceDetail__');
+            Route::put('klinik/drugs/{drug}/reduceDetail', [DrugsController::class, 'updateDetailReduce'])->name('drugs.update1_');
 
             Route::resource('icdten', IcdtenController::class);
 
