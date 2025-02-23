b5ce331da3e6a63702dd27d8ddf5ca2aaaa28499
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-02-23 07:13:01 +0700

    feat(examinations): tambahkan pengolahan observasi untuk pemeriksaan vital
    - Menambahkan metode setObservation untuk memproses data observasi.
    - Mengatur status, kategori, dan subjek observasi berdasarkan data pemeriksaan.
    - Menambahkan komponen untuk tekanan darah, detak jantung, suhu, dan laju pernapasan.
    - Memastikan observasi diposting setelah data pemeriksaan divalidasi dan disimpan.

--- app/Http/Controllers/Klinik/VitalityExaminationsController.php
+++ app/Http/Controllers/Klinik/VitalityExaminationsController.php
@@ -6,10 +6,12 @@
     use App\Http\Controllers\Controller;
     use App\Http\Requests\Klinik\StoreVitalityExaminationRequest;
     use App\Http\Requests\Klinik\UpdateVitalityExaminationRequest;
+    use App\Models\Klinik\Examination;
     use App\Models\Klinik\VitalityExamination;
     use Doctrine\DBAL\Driver\PDO\Exception;
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\Auth;
+    use Satusehat\Integration\FHIR\Observation;
 
 
     class VitalityExaminationsController extends Controller
@@ -54,10 +56,17 @@
             // Validation Data
             $validated = $request->validated();
 
+            //dd($validated);
+
             // Process Data
             if ($validated) {
                 try {
-                    VitalityExamination::create($validated);
+                    $vit = VitalityExamination::create($validated);
+
+                    if($vit) {
+                        $this->setObservation($vit);
+                    }
+
                 } catch (Exception $e) {
                     report($e);
                     return false;
@@ -135,6 +144,13 @@
                 // Process Data
                 try {
                     $vitalityexamination->update($validated);
+
+                    $vit = $vitalityexamination;
+
+                    if($vit) {
+                        $this->setObservation($vit);
+                    }
+
                 } catch (Exception $e) {
                     report($e);
                     return false;
@@ -181,5 +197,69 @@
             }
 
             return redirect()->route('examinations.vitality', ['id'=>$request->examination_id])->with('success', 'Vitality Examination has been updated !!');
+        }
+
+        private function setObservation($vit){
+            $examination         = Examination::where('id', $vit->examination_id)->first();;
+            $_encounter = json_decode($examination->encounter);
+            $participant = $_encounter->participant[0]->individual;
+
+            $observation = new Observation();
+            $observation->setStatus('final');
+            $observation->addCategory('vital-signs');
+            $observation->setSubject(str_replace('Patient/', '', $_encounter->subject->reference), $_encounter->subject->display);
+            $observation->setPerformer(str_replace('Practitioner/','',$participant->reference),$participant->display); // ID SATUSEHAT Dokter, Nama Dokter)
+            $observation->setEncounter($examination->encounter_id);
+
+            if($vit->blood_pressure){
+                $bloodPressure = explode('/', $vit->blood_pressure);
+                foreach ($bloodPressure as $key => $value) {
+                    $observation->addCode($key ? '8462-4' : '8480-6');
+                    $observation->addComponent([
+                        'value'  => (float) filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+                        'unit'   => 'mm[Hg]',
+                        'system' => 'http://unitsofmeasure.org',
+                        'code'   => 'mm[Hg]'
+
+                    ]);
+                    $observation->post();
+                }
+            }
+
+            if($vit->heart_rate){
+                $observation->addCode('8867-4');
+                $observation->addComponent([
+                    'value'  => (float) filter_var($vit->heart_rate, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+                    'unit'   => '{beats}/min',
+                    'system' => 'http://unitsofmeasure.org',
+                    'code'   => '{beats}/min'
+
+                ]);
+                $observation->post();
+            }
+
+            if($vit->temperature){
+                $observation->addCode('8310-5');
+                $observation->addComponent([
+                    'value'  => (float) filter_var($vit->temperature, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+                    'unit'   => 'Cel',
+                    'system' => 'http://unitsofmeasure.org',
+                    'code'   => 'Cel'
+
+                ]);
+                $observation->post();
+            }
+
+            if($vit->respiratory_rate){
+                $observation->addCode('9279-1');
+                $observation->addComponent([
+                    'value'  => (float) filter_var($vit->respiratory_rate, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
+                    'unit'   => '{breaths}/min',
+                    'system' => 'http://unitsofmeasure.org',
+                    'code'   => '{breaths}/min'
+
+                ]);
+                $observation->post();
+            }
         }
     }
