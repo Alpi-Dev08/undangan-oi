abf483e0569bce0d989eabe8bb91229e2a33e1d9
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-03-23 05:24:11 +0700

    feat(profile): gabungkan halaman overview dan details
    - Menggabungkan elemen-elemen dari _navbar.blade.php ke dalam _details.blade.php
    - Menyederhanakan struktur halaman overview dengan menghapus referensi ke _navbar
    - Meningkatkan tampilan profil pengguna dengan menambahkan foto dan informasi singkat
    - Menambahkan tombol-tombol aksi untuk fitur terkait pasien
    - Mengimplementasikan navigasi tab untuk akses cepat ke berbagai bagian profil
    - Merestrukturisasi tampilan detail profil menjadi dua kolom untuk kemudahan membaca
    - Meningkatkan responsivitas dan konsistensi desain secara keseluruhan
    - Perubahan ini bertujuan untuk menciptakan tampilan profil yang lebih terpadu dan user-friendly, mengurangi redundansi kode, dan meningkatkan pengalaman pengguna dalam mengakses informasi dan fitur-fitur terkait akun mereka

--- resources/views/pages/account/overview/_details.blade.php
+++ resources/views/pages/account/overview/_details.blade.php
@@ -1,217 +1,118 @@
-<!--begin::details View-->
-<div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
-    <!--begin::Card header-->
-    <div class="card-header cursor-pointer">
-        <!--begin::Card title-->
-        <div class="card-title m-0">
-            <h3 class="fw-bolder m-0">{{ __('Profile Details') }}</h3>
-        </div>
-        <!--end::Card title-->
+@php
+    $nav = array(
+        array('title' => 'Overview', 'view' => 'account/overview?id='.$user->id),
+        array('title' => 'Settings', 'view' => 'account/settings?id='.$user->id),
+    );
 
+    if($user->hasRole('patient')){
+        $nav[] = array('title' => 'Rekam Medis', 'view' => 'account/medical-records?id='.$user->id);
+    }
+@endphp
+
+<div class="card shadow-sm mb-5" id="kt_profile_details_view">
+    <div class="card-body pt-9 pb-0">
+        <!--begin::Details-->
+        <div class="d-flex flex-wrap flex-sm-nowrap mb-6">
+            <!--begin: Pic-->
+            <div class="me-7 mb-4">
+                <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
+                    <img src="{{ $user->avatar_url }}" alt="image"/>
+                    <div class="position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-white h-20px w-20px"></div>
+                </div>
+            </div>
+            <!--end::Pic-->
+
+            <!--begin::Info-->
+            <div class="flex-grow-1">
+                <div class="d-flex justify-content-between align-items-start flex-wrap mb-2">
+                    <div class="d-flex flex-column">
+                        <div class="d-flex align-items-center mb-2">
+                            <span class="text-gray-800 fs-2 fw-bolder me-1">{{ ($info->title_prefix !='' ? $info->title_prefix.'. ' : '').$user->name.($info->title_suffix!='' ? ', '.$info->title_suffix : '') }}</span>
+                        </div>
+                        <div class="d-flex flex-column fw-bold fs-6 mb-4 pe-2">
+                            @if($user->hasRole('patient'))
+                                <span>{!! $user->patient->patient_code ?? ""  !!}</span>
+                                <span>IHS Number : {!! $user->patient->his_number ?? '-'   !!}</span>
+                            @endif
+                        </div>
+                    </div>
+                    <div class="d-flex my-4">
+                        @if($user->hasRole('patient'))
+                            <a href="{{ theme()->getPageUrl('klinik/laboratoryexamination-lab?id='.$user->id) }}" class="btn btn-sm btn-success me-2">{{ __('Lab Examination') }}</a>
+                            <a href="{{ theme()->getPageUrl('account/examinations?id='.$user->id) }}" class="btn btn-sm btn-success me-2">{{ __('Examination') }}</a>
+                            <a href="{{ theme()->getPageUrl('account/appointments?id='.$user->id) }}" class="btn btn-sm btn-info me-2">{{ __('Appointment') }}</a>
+                        @endif
+                        <a href="{{ theme()->getPageUrl('account/settings?id='.$user->id) }}" class="btn btn-sm btn-primary me-3">{{ __('Edit Profile') }}</a>
+                    </div>
+                </div>
+            </div>
+            <!--end::Info-->
+        </div>
+        <!--end::Details-->
+
+        <!--begin::Navs-->
+        <div class="d-flex overflow-auto h-55px">
+            <ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder flex-nowrap">
+                @foreach($nav as $each)
+                    <li class="nav-item">
+                        <a class="nav-link text-active-primary me-6 {{ theme()->getPagePath() === $each['view'] ? 'active' : '' }}" href="{{ $each['view'] ? theme()->getPageUrl($each['view']) : '#' }}">
+                            {{ $each['title'] }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        <!--end::Navs-->
     </div>
-    <!--begin::Card header-->
 
-    <!--begin::Card body-->
-    <div class="card-body p-9">
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Card Type') }}</label>
-            <!--end::Label-->
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ isset($info->card_type_id) ? $info->card->name : '' }}</span>
+    <div class="card-body pt-9">
+        <div class="row g-5">
+            <div class="col-md-6">
+                @php
+                    $leftDetails = [
+                        'Card Type' => isset($info->card_type_id) ? $info->card->name : '',
+                        'Card Number' => $info->card_number,
+                        'IHS Number' => $user->patient->his_number ?? "-",
+                        'Full Name' => ($info->title_prefix !='' ? $info->title_prefix.'. ' : '').$user->name.($info->title_suffix!='' ? ', '.$info->title_suffix : ''),
+                        'Email' => $user->email,
+                        'Contact Phone' => $user->phone,
+                        'Place and date of Birth' => $info->place_of_birth.', '.$info->date_of_birth,
+                        'Religion' => isset($info->religion) ? $info->religion->name : ''
+                    ];
+                @endphp
+
+                @foreach($leftDetails as $label => $value)
+                    <div class="d-flex flex-column mb-5">
+                        <span class="text-muted fw-bold mb-1">{{ __($label) }}</span>
+                        <span class="fw-bolder fs-6">{{ $value }}</span>
+                    </div>
+                @endforeach
             </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
+            <div class="col-md-6">
+                @php
+                    $rightDetails = [
+                        'Gender' => isset($info->gender) ? $info->gender->name : '',
+                        'Marital Status' => isset($info->marital_status_id) ? $info->marital->name : '',
+                        'Education' => isset($info->education_id) ? $info->education->name : '',
+                        'Work' => isset($info->work_id) ? $info->work->name : '',
+                        'Blood Type' => isset($info->blood_type_id) ? $info->blood->name : '',
+                        'Address' => $info->address.
+                            (isset($info->subdistrict) ? ', '.$info->subdistrict->name : '').
+                            (isset($info->district) ? ', '.$info->district->name : '').
+                            (isset($info->city) ? ', '.$info->city->name : '').
+                            (isset($info->province) ? ', '.$info->province->name : '').
+                            (isset($info->country) ? ', '.$info->country->name : '').
+                            ($info->postal_code!='' ? $info->postal_code : (isset($info->subdistrict) ? ' - '.$info->subdistrict->postal_code : ''))
+                    ];
+                @endphp
 
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Card Number') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ $info->card_number }}</span>
+                @foreach($rightDetails as $label => $value)
+                    <div class="d-flex flex-column mb-5">
+                        <span class="text-muted fw-bold mb-1">{{ __($label) }}</span>
+                        <span class="fw-bolder fs-6">{{ $value }}</span>
+                    </div>
+                @endforeach
             </div>
-            <!--end::Col-->
         </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('IHS Number') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ $user->patient->his_number ?? "-" }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Row-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Full Name') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8">
-                <span
-                    class="fw-bolder fs-6 text-dark">{{ ($info->title_prefix !='' ? $info->title_prefix.'. ' : '').$user->name.($info->title_suffix!='' ? ', '.$info->title_suffix : '') }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Row-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Email') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ $user->email }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">
-                {{ __('Contact Phone') }}
-                <i class="fas fa-exclamation-circle ms-1 fs-7" data-bs-toggle="tooltip"
-                   title="Phone number must be active"></i>
-            </label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 d-flex align-items-center">
-                <span class="fw-bolder fs-6 me-2">{{ $user->phone }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Place and date of Birth') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ $info->place_of_birth.', '.$info->date_of_birth }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Religion') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ isset($info->religion) ? $info->religion->name : '' }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Gender') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ isset($info->gender) ? $info->gender->name : '' }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Marital Status') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ isset($info->marital_status_id) ? $info->marital->name : '' }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Education') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ isset($info->education_id) ? $info->education->name : '' }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Work') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ isset($info->work_id) ? $info->work->name : '' }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Blood Type') }}</label>
-            <!--end::Label-->
-            <!--begin::Col-->
-            <div class="col-lg-8 fv-row">
-                <span class="fw-bold fs-6">{{ isset($info->blood_type_id) ? $info->blood->name : '' }}</span>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
-        <!--begin::Input group-->
-        <div class="row mb-7">
-            <!--begin::Label-->
-            <label class="col-lg-4 fw-bold text-muted">{{ __('Address') }}</label>
-            <!--end::Label-->
-
-            <!--begin::Col-->
-            <div class="col-lg-8">
-                <a href="#"
-                   class="fw-bold fs-6 text-dark text-hover-primary">{{ $info->address }}{{ isset($info->subdistrict) ? ', '.$info->subdistrict->name : '' }}{{ isset($info->district) ? ', '.$info->district->name : '' }}{{ isset($info->city) ? ', '.$info->city->name : '' }}{{ isset($info->province) ? ', '.$info->province->name : '' }}{{ isset($info->country) ? ', '.$info->country->name : '' }}{{ $info->postal_code!='' ? $info->postal_code : (isset($info->subdistrict) ? ' - '.$info->subdistrict->postal_code : '') }}</a>
-            </div>
-            <!--end::Col-->
-        </div>
-        <!--end::Input group-->
     </div>
-    <!--end::Card body-->
 </div>
-<!--end::details View-->
--- resources/views/pages/account/overview/overview.blade.php
+++ resources/views/pages/account/overview/overview.blade.php
@@ -1,5 +1,3 @@
 <x-base-layout>
-{{ theme()->getView('pages/account/_navbar', array('class' => 'mb-5 mb-xl-10', 'user' => $user,  'info' => $user->info)) }}
-
 {{ theme()->getView('pages/account/overview/_details', array('class' => 'mb-5 mb-xl-10', 'user' => $user, 'info' => $user->info)) }}
 </x-base-layout>
