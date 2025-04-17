06dc3863c81612e41213b2eb3378ff26ab74a14f
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-04-17 09:56:10 +0700

    feat(examinations): perbarui informasi organisasi pada dokumen persetujuan
    - Mengganti informasi alamat dan kontak organisasi dengan fungsi dinamis.
    - Memperbaiki format teks pada bagian pernyataan.
    - Menghapus baris kosong yang tidak perlu.

--- resources/views/pages/klinik/examinations/persetujuan.blade.php
+++ resources/views/pages/klinik/examinations/persetujuan.blade.php
@@ -73,10 +73,7 @@
             </td>
             <td style="width: 50%; vertical-align:top">
                 <p style="margin:0px; margin-top:10px; font-size:12px;text-align: right;color:#000;">
-                    Ruko C17, Pasar Intermoda BSD<br>
-                    Sampora, Cisauk, Kab. Tangerang, Banten - 15414<br>
-                    +62 21 5020 8805 - klinik@dharma.or.id<br>
-                    https://klinik.dharma.or.id
+                    {!! organizationInfo('full') !!}
                 </p>
             </td>
         </tr>
@@ -120,7 +117,7 @@
     </table>
 
     <p>Dengan ini menyatakan dengan sesungguhnya telah memberikan pernyataan :</p>
-    
+
     <p><strong>{{ $data->persetujuan }} dilakukan tindakan medis</strong></p>
 
     @if($data->persetujuan === 'Tidak Setuju')
@@ -184,7 +181,7 @@
     </table>
 
     <p>
-        Yang tujuan, sifat dan perlunya tindakan medis tersebut diatas, serta risiko yang dapat 
+        Yang tujuan, sifat dan perlunya tindakan medis tersebut diatas, serta risiko yang dapat
         ditimbulkannya telah cukup dijelaskan oleh dokter dan telah saya mengerti sepenuhnya.
     </p>
     <p>
