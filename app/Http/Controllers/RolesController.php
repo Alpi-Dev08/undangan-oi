9e46c24c46b5c03d45af0abe00be83d0ab6c5a1f
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-05-18 12:47:23 +0700

    refactor(roles): optimalkan RolesController dengan abstraksi izin, validasi, dan pembaruan nama variabel
    - Menambahkan properti `guardName` untuk mempermudah pengaturan guard.
    - Menerapkan metode `authorizeUser` untuk mempermudah proses otorisasi pengguna dengan pesan error kustom.
    - Perbaikan dan penyederhanaan pada validasi form dengan pesan error yang lebih jelas dalam Bahasa Indonesia.
    - Mengubah nama variabel menjadi lebih deskriptif, seperti `permissiongroups` menjadi `permissionGroups` untuk konsistensi.
    - Menghapus redundansi kode terkait otorisasi pengguna dan memperbaikinya dengan abstraksi yang lebih modular.
    - Menambahkan konsistensi pada flash message serta mengoptimalkan logika sinkronisasi role dan permission.
    - Meningkatkan keterbacaan kode dengan format yang lebih rapi pada proses operasi CRUD.

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -1,177 +1,170 @@
 <?php
 
-    namespace App\Http\Controllers;
 
-    use App\DataTables\RolesDataTable;
-    use App\Http\Controllers\Controller;
-    use App\Models\PermissionGroup;
-    use App\Models\User;
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Auth;
-    use Illuminate\Support\Facades\DB;
-    use Spatie\Permission\Models\Role;
-    use App\Models\Permission;
+class RolesController extends Controller
+{
+    protected $user;
+    protected $guardName = 'web';
 
-    class RolesController extends Controller
+    public function __construct()
     {
-        public $user;
+        $this->middleware(function ($request, $next) {
+            $this->user = Auth::guard($this->guardName)->user();
+            return $next($request);
+        });
+    }
 
-        public function __construct()
-        {
-            $this->middleware(function ($request, $next) {
-                $this->user = Auth::guard('web')->user();
-                return $next($request);
-            });
-        }
-
-        /**
-         * Display a listing of the resource.
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function index(RolesDataTable $dataTable)
-        {
-            if (is_null($this->user) || !$this->user->can('role.read')) {
-                abort(403, 'Sorry !! You are Unauthorized to view any role !');
-            }
-            return $dataTable->render('pages.roles.index');
-        }
-
-        /**
-         * Show the form for creating a new resource.
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function create()
-        {
-            if (is_null($this->user) || !$this->user->can('role.create')) {
-                abort(403, 'Sorry !! You are Unauthorized to create any role !');
-            }
-
-            $permissiongroups = PermissionGroup::all();
-            return view('pages.roles.create',compact(['permissiongroups']));
-        }
-
-        /**
-         * Store a newly created resource in storage.
-         *
-         * @param \Illuminate\Http\Request $request
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function store(Request $request)
-        {
-            if (is_null($this->user) || !$this->user->can('role.create')) {
-                abort(403, 'Sorry !! You are Unauthorized to create any role !');
-            }
-
-            // Validation Data
-            $request->validate([
-                'name' => 'required|max:100|unique:roles'
-            ], [
-                'name.requried' => 'Please give a role name'
-            ]);
-
-            // Process Data
-            $role = Role::create(['name' => $request->name, 'guard_name' => 'web']);
-
-            $permissions = $request->input('permissions');
-
-            if (!empty($permissions)) {
-                $role = Role::find($role->id);
-                $role->syncPermissions($permissions);
-            }
-
-            session()->flash('success', 'Role has been created !!');
-            return redirect()->route('roles.index');
-        }
-
-        /**
-         * Display the specified resource.
-         *
-         * @param int $id
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function show($id)
-        {
-            //
-        }
-
-        /**
-         * Show the form for editing the specified resource.
-         *
-         * @param int $id
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function edit($id)
-        {
-            if (is_null($this->user) || !$this->user->can('role.update')) {
-                abort(403, 'Sorry !! You are Unauthorized to edit any role !');
-            }
-
-            $role              = Role::findById($id, 'web');
-            $all_permissions   = Permission::all();
-            $permissiongroups = PermissionGroup::all();
-            return view('pages.roles.edit', compact('role', 'all_permissions','permissiongroups'));
-        }
-
-        /**
-         * Update the specified resource in storage.
-         *
-         * @param \Illuminate\Http\Request $request
-         * @param int                      $id
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function update(Request $request, $id)
-        {
-            if (is_null($this->user) || !$this->user->can('role.update')) {
-                abort(403, 'Sorry !! You are Unauthorized to edit any role !');
-            }
-
-            // Validation Data
-            $request->validate([
-                'name' => 'required|max:100|unique:roles,name,' . $id
-            ], [
-                'name.requried' => 'Please give a role name'
-            ]);
-
-            $role        = Role::findById($id, 'web');
-            $permissions = $request->input('permissions');
-
-            $role->name = $request->name;
-            $role->save();
-
-            if (!empty($permissions)) {
-                $role->syncPermissions($permissions);
-            }
-
-            session()->flash('success', 'Role has been updated !!');
-            return redirect()->route('roles.index');
-        }
-
-        /**
-         * Remove the specified resource from storage.
-         *
-         * @param int $id
-         *
-         * @return \Illuminate\Http\Response
-         */
-        public function destroy($id)
-        {
-            if (is_null($this->user) || !$this->user->can('role.delete')) {
-                abort(403, 'Sorry !! You are Unauthorized to delete any role !');
-            }
-
-
-            $role = Role::findById($id, 'web');
-            if (!is_null($role)) {
-                $role->delete();
-            }
-
-            session()->flash('success', 'Role has been deleted !!');
-            return redirect()->route('roles.index');
+    /**
+     * Memeriksa apakah pengguna memiliki izin tertentu
+     *
+     * @param string $permission izin yang diperlukan
+     * @param string $message pesan error untuk ditampilkan
+     * @return void
+     */
+    protected function authorizeUser(string $permission, string $message)
+    {
+        if (is_null($this->user) || !$this->user->can($permission)) {
+            abort(403, $message);
         }
     }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(RolesDataTable $dataTable)
+    {
+        $this->authorizeUser('role.read', 'Maaf !! Anda tidak berwenang untuk melihat role apapun!');
+        return $dataTable->render('pages.roles.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $this->authorizeUser('role.create', 'Maaf !! Anda tidak berwenang untuk membuat role apapun!');
+
+        $permissionGroups = PermissionGroup::all();
+        return view('pages.roles.create', compact('permissionGroups'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->authorizeUser('role.create', 'Maaf !! Anda tidak berwenang untuk membuat role apapun!');
+
+        // Validation Data
+        $request->validate([
+            'name' => 'required|max:100|unique:roles'
+        ], [
+            'name.required' => 'Harap berikan nama role'
+        ]);
+
+        // Process Data
+        $role = Role::create([
+            'name' => $request->name,
+            'guard_name' => $this->guardName
+        ]);
+
+        $permissions = $request->input('permissions');
+        if (!empty($permissions)) {
+            $role->syncPermissions($permissions);
+        }
+
+        session()->flash('success', 'Role telah berhasil dibuat!');
+        return redirect()->route('roles.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $this->authorizeUser('role.update', 'Maaf !! Anda tidak berwenang untuk mengedit role apapun!');
+
+        $role = Role::findById($id, $this->guardName);
+        $allPermissions = Permission::all();
+        $permissionGroups = PermissionGroup::all();
+
+        return view('pages.roles.edit', compact('role', 'allPermissions', 'permissionGroups'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->authorizeUser('role.update', 'Maaf !! Anda tidak berwenang untuk mengedit role apapun!');
+
+        // Validation Data
+        $request->validate([
+            'name' => 'required|max:100|unique:roles,name,' . $id
+        ], [
+            'name.required' => 'Harap berikan nama role'
+        ]);
+
+        $role = Role::findById($id, $this->guardName);
+        $permissions = $request->input('permissions');
+
+        $role->name = $request->name;
+        $role->save();
+
+        if (!empty($permissions)) {
+            $role->syncPermissions($permissions);
+        }
+
+        session()->flash('success', 'Role telah berhasil diperbarui!');
+        return redirect()->route('roles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->authorizeUser('role.delete', 'Maaf !! Anda tidak berwenang untuk menghapus role apapun!');
+
+        $role = Role::findById($id, $this->guardName);
+        if (!is_null($role)) {
+            $role->delete();
+        }
+
+        session()->flash('success', 'Role telah berhasil dihapus!');
+        return redirect()->route('roles.index');
+    }
+}
