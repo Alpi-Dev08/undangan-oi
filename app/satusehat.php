a2fbc70ebf390f850b09b0a240f3e3a7a1229808
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-06-03 08:37:00 +0700

    fix(satusehat): sesuaikan environment variable untuk akses token di produksi
    - Mengubah nama environment variable:
      - `SATU_SEHAT_CLIENT_ID` menjadi `CLIENTID_PROD`.
      - `SATU_SEHAT_CLIENT_SECRET` menjadi `CLIENTSECRET_PROD`.
      - `SATU_SEHAT_AUTH_URL` menjadi `SATUSEHAT_BASE_URL_PROD`.
    
    - Penyesuaian URL endpoint:
      - Menambahkan parameter endpoint `'/accesstoken?grant_type=client_credentials'` ke URL environment baru.
    
    - Tujuan perubahan:
      - Memastikan pengambilan token akses menggunakan konfigurasi environment variabel produksi.
      - Menghindari penggunaan environment variabel non-produksi pada operasi di lingkungan live/produksi.

--- app/satusehat.php
+++ app/satusehat.php
@@ -6,9 +6,9 @@
 
     function generateToken()
     {
-        $clientId     = env('SATU_SEHAT_CLIENT_ID');
-        $clientSecret = env('SATU_SEHAT_CLIENT_SECRET');
-        $url          = env('SATU_SEHAT_AUTH_URL') . '/accesstoken?grant_type=client_credentials';
+        $clientId     = env('CLIENTID_PROD');
+        $clientSecret = env('CLIENTSECRET_PROD');
+        $url          = env('SATUSEHAT_BASE_URL_PROD') . '/accesstoken?grant_type=client_credentials';
 
         $data    = [
             'client_id'     => $clientId,
