fca0913a61c02d9aac9cf6421cbe0e23a39ce173
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-06-04 10:04:13 +0700

    feat(FHIR): tambahkan dukungan kategori dan komponen baru pada Observations
    - **Penambahan kategori baru**:
      - Menambahkan metode `addCategory()` pada kelas `Observations` untuk menambahkan kategori seperti `vital-signs` dan `exam`.
      - Setiap kategori memiliki properti `coding` yang mencakup `system`, `code`, dan `display`.
    
    - **Penambahan kode observasi baru**:
      - Menambahkan dukungan untuk kode observasi `Physical findings of Head Narrative` (`10199-8`).
      - Menambahkan dukungan untuk kode observasi `Physical findings of Eye Narrative` (`10197-2`).
    
    - **Keuntungan utama**:
      - Memungkinkan fleksibilitas dalam menambahkan kategori observasi berdasarkan standar HL7 FHIR.
      - Memberikan dukungan kode observasi baru untuk narasi temuan fisik, meningkatkan cakupan data klinis yang dapat dikelola.
      - Menambah kemampuan pemrosesan data observasi untuk kebutuhan pemeriksaan medis lebih mendalam.

--- app/FHIR/Observations.php
+++ app/FHIR/Observations.php
@@ -44,6 +44,14 @@ class Observations extends Observation{
                 $display = 'Body weight';
                 $code = '29463-7';
                 break;
+            case '10199-8':
+                $display = 'Physical findings of Head Narrative';
+                $code = '10199-8';
+                break;
+            case '10197-2':
+                $display = 'Physical findings of Eye Narrative';
+                $code = '10197-2';
+                break;
 
         }
 
@@ -63,6 +71,41 @@ class Observations extends Observation{
     public function addComponent(array $observationComponent) : Observation
     {
         $this->observation['valueQuantity'] = $observationComponent;
+
+        return $this;
+    }
+
+    /**
+     * Adds a category to the observation.
+     *
+     * @param  string  $category  the code of the category
+     * @return Observation The updated observation object.
+     */
+    public function addCategory(string $category): Observation
+    {
+        $display = '';
+        $code = '';
+        switch ($category) {
+            case 'vital-signs':
+                $display = 'Vital Signs';
+                $code = 'vital-signs';
+                break;
+            case 'exam':
+                $display = 'Exam';
+                $code = 'exam';
+                break;
+        }
+
+        // NOTE: we currently only support 'vital-signs'
+        $this->observation['category'][] = [
+            'coding' => [
+                [
+                    'system' => 'http://terminology.hl7.org/CodeSystem/observation-category',
+                    'code' => $code,
+                    'display' => $display,
+                ],
+            ],
+        ];
 
         return $this;
     }
