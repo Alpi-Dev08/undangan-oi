3e31f199c882953e9fd62578ed0460b3d6514376
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-06-29 16:52:26 +0700

    feat(view): tambahkan tab profil pasien di halaman pemeriksaan klinik
    - **Penambahan Komponen Baru**:
      - Menambahkan tab baru bernama `user` di halaman pemeriksaan klinik untuk menampilkan profil pasien secara lengkap.
      - Tab ini mencakup informasi berikut:
        1. Avatar pasien beserta status aktif/inaktif.
        2. Informasi dasar pasien seperti ID, Rekam Medis, Nama, Email, dan Nomor Telepon.
        3. Informasi personal termasuk tempat & tanggal lahir, jenis kelamin, agama, dan golongan darah.
        4. Informasi fisik seperti tinggi badan dan berat badan.
        5. Informasi alamat pasien beserta rincian seperti kota, provinsi, dan kode pos.
    
    - **Struktur File**:
      - Menggunakan partial Blade berikut untuk memisahkan setiap bagian informasi:
        - `_patient-avatar.blade.php`
        - `_patient-basic-info.blade.php`
        - `_patient-personal-info.blade.php`
        - `_patient-physical-info.blade.php`
        - `_patient-address-info.blade.php`

---
+++ resources/views/pages/klinik/examinations/partials/_patient-profile-tab.blade.php
@@ -0,0 +1,11 @@
+<div class="tab-pane" id="user" role="tabpanel" aria-labelledby="all-tab" data-kt-timeline-widget-4-blockui="true">
+    <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
+        <div class="card-body p-9">
+            @include('pages.klinik.examinations.partials._patient-avatar')
+            @include('pages.klinik.examinations.partials._patient-basic-info')
+            @include('pages.klinik.examinations.partials._patient-personal-info')
+            @include('pages.klinik.examinations.partials._patient-physical-info')
+            @include('pages.klinik.examinations.partials._patient-address-info')
+        </div>
+    </div>
+</div>
