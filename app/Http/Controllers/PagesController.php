7426c7d11d33f5235a15e38325ed544c81427d78
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-07-25 09:10:38 +0700

    refactor(controllers): Perbaiki routing dan cleanup kode controller
    - Update redirect setelah logout ke dashboard di AuthenticatedSessionController
    - Hapus method index dan replaceIcons yang tidak terpakai di PagesController
    - Cleanup whitespace dan formatting di PagesController
    - Perbaiki tipe data pagination dari array ke object di PcareController
    - Optimasi struktur kode untuk meningkatkan maintainability
    - Menghapus kode yang redundan dan tidak digunakan
    - Standardisasi format response pagination untuk konsistensi API

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,64 +11,6 @@
 
     class PagesController extends Controller
     {
-        /**
-         * Display a listing of the resource.
-         *
-         * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-         */
-        public function index(Request $request)
-        {
-
-            // Get view file location from menu config
-            $view = theme()->getOption('page', 'view');
-            // Check if the page view file exist
-            if (view()->exists('pages.' . $view)) {
-                if ($request->id) {
-                    $user = User::where('id', $request->id)->first();
-                } else {
-                    $user = Auth::user();
-                }
-                $kyc_iframe = true;
-
-                return view('pages.' . $view, compact('user','kyc_iframe'));
-            }
-
-            // Get the default inner page
-            return redirect('/');
-        }
-
-        /**
-         * Temporary function to replace icon duotone
-         */
-        public function replaceIcons()
-        {
-            $fileContent = file_get_contents(public_path('icon_replacement.txt'));
-            $lines       = explode("\n", $fileContent);
-
-            $patterns     = [];
-            $replacements = [];
-            foreach ($lines as $line) {
-                $el = explode(' - ', $line);
-                if (empty($line)) {
-                    continue;
-                }
-                $patterns[]     = trim($el[0]);
-                $replacements[] = trim($el[1]);
-            }
-
-            $files    = File::allFiles(resource_path());
-            $filtered = array_filter($files, function ($str) {
-                return strpos($str, ".php") !== false;
-            });
-
-            foreach ($filtered as $file) {
-                $bladeFileContent = file_get_contents($file->getPathname());
-
-                $bladeFileContent = str_replace($patterns, $replacements, $bladeFileContent);
-
-                file_put_contents($file->getPathname(), $bladeFileContent);
-            }
-        }
 
         public function bukti_penyampaian_informasi($id)
         {
@@ -101,7 +43,6 @@
         public function store_bukti_penyampaian_informasi(Request $request)
         {
             $examination = Examination::find($request->id);
-
             $examination->save();
 
             return response()->json([
@@ -109,8 +50,4 @@
                 'message' => 'Bukti Penyampaian Informasi Berhasil Disimpan',
             ]);
         }
-
-
-
-
     }
