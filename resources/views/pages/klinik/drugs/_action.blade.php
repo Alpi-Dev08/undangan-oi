c5efe47030bcf9db01890751cc15d02000c816d0
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-08-24 07:32:53 +0700

    🎨 style(views): Refactor tombol aksi dengan regular form
    - Refactor Form Structure: Mengganti Form::open Laravel Collective menjadi native HTML form untuk konsistensi dengan standar modern
    - Improve Security: Menambahkan @csrf dan @method('DELETE') secara eksplisit untuk proteksi CSRF dan HTTP method spoofing
    - Code Formatting: Memperbaiki indentasi dan spacing untuk meningkatkan readability kode
    - Quote Consistency: Mengubah semua quote dari double (") menjadi single (') untuk konsistensi gaya kode
    - Semantic HTML: Menggunakan <form> tag yang lebih semantik daripada Laravel Collective Form facade
    
    Perubahan ini tidak mempengaruhi fungsionalitas, hanya refactor struktur form untuk meningkatkan konsistensi dan maintainability kode.

--- resources/views/pages/klinik/drugs/_action.blade.php
+++ resources/views/pages/klinik/drugs/_action.blade.php
@@ -1,25 +1,29 @@
 <div class="d-flex flex-row flex-center">
-    @if(Auth::user()->can('klinik.update'))
-        <a href="{{ route('drugs.edit',['drug' => $model->id]) }}"
-           class="btn btn-icon btn-bg-light  btn-active-light-primary btn-sm me-1">
-            {!! theme()->getSvgIcon("icons/duotune/art/art005.svg", "svg-icon-3") !!}
-        </a>
-    @endif
-    @if(Auth::user()->can('klinik.update'))
-        <a href="{{ route('drugs.detail', ['drug' => $model->id]) }}"
-        class="btn btn-icon btn-bg-light btn-active-light-primary btn-sm me-1">
-        <i class="bi bi-plus-circle-fill svg-icon-3"></i>
+    @if (Auth::user()->can('klinik.update'))
+        <a href="{{ route('drugs.edit', ['drug' => $model->id]) }}"
+            class="btn btn-icon btn-bg-light  btn-active-light-primary btn-sm me-1">
+            {!! theme()->getSvgIcon('icons/duotune/art/art005.svg', 'svg-icon-3') !!}
         </a>
     @endif
     @if (Auth::user()->can('klinik.update'))
-         <a href="{{ route('drugs.reduceDetail', ['drug' => $model->id]) }}"
-        class="btn btn-icon btn-bg-light btn-active-light-primary btn-sm me-1">
-        <i class="bi bi-dash-circle-fill svg-icon-3"></i>
-        </a>               
+        <a href="{{ route('drugs.detail', ['drug' => $model->id]) }}"
+            class="btn btn-icon btn-bg-light btn-active-light-primary btn-sm me-1">
+            <i class="bi bi-plus-circle-fill svg-icon-3"></i>
+        </a>
     @endif
-    @if(Auth::user()->can('klinik.delete'))
-        {!! Form::open(['method' => 'DELETE','route' => ['drugs.destroy', $model->id],'class'=>'']) !!}
-        {{ Form::button(theme()->getSvgIcon("icons/duotune/general/gen027.svg", "svg-icon-3"), ['type' => 'submit', 'class' => 'delete btn btn-icon btn-bg-light btn-active-light-danger btn-sm'] )  }}
-        {!! Form::close() !!}
+    @if (Auth::user()->can('klinik.update'))
+        <a href="{{ route('drugs.reduceDetail', ['drug' => $model->id]) }}"
+            class="btn btn-icon btn-bg-light btn-active-light-primary btn-sm me-1">
+            <i class="bi bi-dash-circle-fill svg-icon-3"></i>
+        </a>
+    @endif
+    @if (Auth::user()->can('klinik.delete'))
+        <form method="POST" action="{{ route('drugs.destroy', $model->id) }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="delete btn btn-icon btn-bg-light btn-active-light-danger btn-sm">
+                {!! theme()->getSvgIcon('icons/duotune/general/gen027.svg', 'svg-icon-3') !!}
+            </button>
+        </form>
     @endif
 </div>
