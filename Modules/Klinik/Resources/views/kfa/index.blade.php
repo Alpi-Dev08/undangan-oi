ecd2acd5c118d1f0ee62502091891542ec5b2948
Daeng Deni Mardaeni <ddeni05@gmail.com>
2025-08-25 10:05:28 +0700

    ✨(klinik): Perbarui tampilan modul KFA mengikuti desain modul drugs
    - Mengubah layout dari @extends('layouts.app') menjadi <x-base-layout> untuk konsistensi dengan tema Metronic
    - Memperbarui struktur card dan tombol aksi mengikuti pola modul drugs
    - Menambahkan ikon SVG dan styling yang konsisten dengan tema yang digunakan
    - Mengubah modal Bootstrap 4 menjadi Bootstrap 5 dengan btn-close
    - Memperbarui script JavaScript menggunakan pola KTUtil.onDOMContentLoaded
    - Menambahkan event handler untuk select2 pada jenis produk
    - Membuat file _table.blade.php untuk tabel DataTables yang reusable
    - Memperbaiki responsive layout dan styling untuk mobile devices

--- Modules/Klinik/Resources/views/kfa/index.blade.php
+++ Modules/Klinik/Resources/views/kfa/index.blade.php
@@ -1,72 +1,79 @@
-@extends('layouts.app')
-
-@section('title', 'Daftar Produk KFA')
-
-@section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Daftar Produk KFA</h3>
-                        <div class="card-tools">
-                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                                <i class="fas fa-minus"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="row mb-3">
-                            <div class="col-md-4">
-                                <label for="productType">Jenis Produk</label>
-                                <select class="form-control" id="productType">
-                                    <option value="farmasi">Farmasi</option>
-                                    <option value="alkes">Alat Kesehatan</option>
-                                </select>
-                            </div>
-                            <div class="col-md-6">
-                                <label for="keyword">Kata Kunci</label>
-                                <input type="text" class="form-control" id="keyword" placeholder="Masukkan nama produk atau kode KFA">
-                            </div>
-                            <div class="col-md-2">
-                                <label>&nbsp;</label>
-                                <button type="button" class="btn btn-primary btn-block" id="searchBtn">
-                                    <i class="fas fa-search"></i> Cari
-                                </button>
-                            </div>
-                        </div>
-                        
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped" id="kfa-products-table" style="width:100%">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Kode KFA</th>
-                                        <th>Nama Produk</th>
-                                        <th>Produsen</th>
-                                        <th>Harga Fix</th>
-                                        <th>HET</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
-                        </div>
-                    </div>
+<x-base-layout>
+    <!--begin::Card-->
+    <div class="card card-xxl-stretch mb-5 mb-xl-8">
+        <!--begin::Card body-->
+        <div class="card-header border-0 pt-5">
+            <h3 class="card-title align-items-start flex-column">
+                <div class="d-flex align-items-center position-relative my-1">
+                    <!--begin::Svg Icon | path: icons/duotune/general/gen021.svg-->
+                    <span class="svg-icon svg-icon-1 position-absolute ms-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none">
+                            <rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1"
+                                transform="rotate(45 17.0365 15.1223)" fill="currentColor"></rect>
+                            <path
+                                d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z"
+                                fill="currentColor"></path>
+                        </svg>
+                    </span>
+                    <!--end::Svg Icon-->
+                    <input type="text" id="searchbox"
+                        class="form-control form-control-solid border border-gray-300 w-250px ps-15"
+                        placeholder="Search KFA Products">
                 </div>
+
+            </h3>
+
+            <div class="card-toolbar" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-trigger="hover"
+                title=""
+                data-bs-original-title="Actions">
+                <a href="{{ route('kfa.products') }}" class="btn btn-sm btn-light-success me-2">
+                    <!--begin::Svg Icon | path: icons/duotune/files/fil003.svg-->
+                    <span class="svg-icon svg-icon-muted svg-icon-2hx">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path opacity="0.3" d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22Z" fill="currentColor"/>
+                            <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="currentColor"/>
+                        </svg>
+                    </span>
+                    <!--end::Svg Icon-->
+                    Export Excel
+                </a>
             </div>
         </div>
+        <div class="card-body pt-6">
+            <div class="row mb-3">
+                <div class="col-md-4">
+                    <label for="productType" class="form-label">Jenis Produk</label>
+                    <select class="form-select form-select-solid" id="productType" data-control="select2" data-hide-search="true">
+                        <option value="farmasi">Farmasi</option>
+                        <option value="alkes">Alat Kesehatan</option>
+                    </select>
+                </div>
+                <div class="col-md-6">
+                    <label for="keyword" class="form-label">Kata Kunci</label>
+                    <input type="text" class="form-control form-control-solid" id="keyword" placeholder="Masukkan nama produk atau kode KFA">
+                </div>
+                <div class="col-md-2">
+                    <label>&nbsp;</label>
+                    <button type="button" class="btn btn-primary w-100" id="searchBtn">
+                        <i class="fas fa-search"></i> Cari
+                    </button>
+                </div>
+            </div>
+            
+            @include('klinik::kfa._table')
+        </div>
+        <!--end::Card body-->
     </div>
+    <!--end::Card-->
 
     <!-- Modal Detail Produk -->
-    <div class="modal fade" id="kfaDetailModal" tabindex="-1" role="dialog" aria-labelledby="kfaDetailModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
+    <div class="modal fade" id="kfaDetailModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="kfaDetailModalLabel">Detail Produk KFA</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                    <h5 class="modal-title">Detail Produk KFA</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div id="kfaDetailContent">
@@ -74,114 +81,148 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
                 </div>
             </div>
         </div>
     </div>
-@endsection
+</x-base-layout>
 
 @push('scripts')
     <script>
-        $(document).ready(function() {
-            let kfaProductsTable = $('#kfa-products-table').DataTable({
-                processing: true,
-                serverSide: false,
-                ajax: {
-                    url: "{{ route('kfa.products') }}",
-                    type: 'GET',
-                    data: function(d) {
-                        d.product_type = $('#productType').val();
-                        d.keyword = $('#keyword').val();
+        "use strict";
+
+        // Class definition
+        var KFAProducts = function () {
+            // Shared variables
+            var table;
+            var datatable;
+
+            // Private functions
+            var initDatatable = function () {
+                datatable = $('#kfa-products-table').DataTable({
+                    processing: true,
+                    serverSide: false,
+                    ajax: {
+                        url: "{{ route('kfa.products') }}",
+                        type: 'GET',
+                        data: function(d) {
+                            d.product_type = $('#productType').val();
+                            d.keyword = $('#keyword').val();
+                        }
+                    },
+                    columns: [
+                        { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
+                        { data: 'kfa_code', name: 'kfa_code' },
+                        { data: 'name', name: 'name' },
+                        { data: 'manufacturer', name: 'manufacturer' },
+                        { data: 'fix_price', name: 'fix_price' },
+                        { data: 'het_price', name: 'het_price' },
+                        { data: 'action', name: 'action', orderable: false, searchable: false }
+                    ],
+                    responsive: true,
+                    autoWidth: false,
+                    language: {
+                        url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/id.json'
                     }
-                },
-                columns: [
-                    { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
-                    { data: 'kfa_code', name: 'kfa_code' },
-                    { data: 'name', name: 'name' },
-                    { data: 'manufacturer', name: 'manufacturer' },
-                    { data: 'fix_price', name: 'fix_price' },
-                    { data: 'het_price', name: 'het_price' },
-                    { data: 'action', name: 'action', orderable: false, searchable: false }
-                ],
-                responsive: true,
-                autoWidth: false,
-                language: {
-                    url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/id.json'
-                }
-            });
+                });
 
-            $('#searchBtn').click(function() {
-                kfaProductsTable.ajax.reload();
-            });
+                table = datatable;
+            }
 
-            $('#keyword').keypress(function(e) {
-                if (e.which == 13) {
-                    kfaProductsTable.ajax.reload();
+            var handleSearchDatatable = function () {
+                $('#searchBtn').on('click', function () {
+                    datatable.ajax.reload();
+                });
+
+                $('#keyword').on('keypress', function (e) {
+                    if (e.which == 13) {
+                        datatable.ajax.reload();
+                    }
+                });
+
+                $('#productType').on('change', function () {
+                    datatable.ajax.reload();
+                });
+            }
+
+            var handleViewDetail = function () {
+                window.viewKfaDetail = function(kfaCode) {
+                    if (!kfaCode) return;
+
+                    $('#kfaDetailContent').html('<div class="text-center"><div class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div> Memuat...</div>');
+                    $('#kfaDetailModal').modal('show');
+
+                    $.ajax({
+                        url: "{{ route('kfa.product-detail') }}",
+                        type: 'GET',
+                        data: { kfa_code: kfaCode },
+                        success: function(response) {
+                            if (response.success && response.data) {
+                                let data = response.data;
+                                let html = `
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <strong>Kode KFA:</strong><br>
+                                            ${data.kfa_code || '-'}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <strong>Nama Produk:</strong><br>
+                                            ${data.name || '-'}
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <strong>Produsen:</strong><br>
+                                            ${data.manufacturer || '-'}
+                                        </div>
+                                        <div class="col-md-6">
+                                            <strong>Negara Produsen:</strong><br>
+                                            ${data.manufacturer_country || '-'}
+                                        </div>
+                                    </div>
+                                    <hr>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <strong>Harga Fix:</strong><br>
+                                            ${data.fix_price ? 'Rp ' + new Intl.NumberFormat('id-ID').format(data.fix_price) : '-'}
+                                        </div>
+                                        <div class="col-md-4">
+                                            <strong>HET:</strong><br>
+                                            ${data.het_price ? 'Rp ' + new Intl.NumberFormat('id-ID').format(data.het_price) : '-'}
+                                        </div>
+                                        <div class="col-md-4">
+                                            <strong>Kemasan:</strong><br>
+                                            ${data.packaging || '-'}
+                                        </div>
+                                    </div>
+                                `;
+                                $('#kfaDetailContent').html(html);
+                            } else {
+                                $('#kfaDetailContent').html('<div class="alert alert-danger">' + (response.message || 'Gagal memuat detail produk.') + '</div>');
+                            }
+                        },
+                        error: function() {
+                            $('#kfaDetailContent').html('<div class="alert alert-danger">Terjadi kesalahan saat memuat data.</div>');
+                        }
+                    });
                 }
-            });
+            }
+
+            // Public methods
+            return {
+                init: function () {
+                    initDatatable();
+                    handleSearchDatatable();
+                    handleViewDetail();
+                }
+            }
+        }();
+
+        // On document ready
+        KTUtil.onDOMContentLoaded(function () {
+            KFAProducts.init();
         });
-
-        function viewKfaDetail(kfaCode) {
-            if (!kfaCode) return;
-
-            $('#kfaDetailContent').html('<div class="text-center"><i class="fas fa-spinner fa-spin"></i> Memuat...</div>');
-            $('#kfaDetailModal').modal('show');
-
-            $.ajax({
-                url: `{{ route('kfa.product-detail') }}`,
-                type: 'GET',
-                data: { kfa_code: kfaCode },
-                success: function(response) {
-                    if (response.success && response.data) {
-                        let data = response.data;
-                        let html = `
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <strong>Kode KFA:</strong><br>
-                                    ${data.kfa_code || '-'}
-                                </div>
-                                <div class="col-md-6">
-                                    <strong>Nama Produk:</strong><br>
-                                    ${data.name || '-'}
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <strong>Produsen:</strong><br>
-                                    ${data.manufacturer || '-'}
-                                </div>
-                                <div class="col-md-6">
-                                    <strong>Negara Produsen:</strong><br>
-                                    ${data.manufacturer_country || '-'}
-                                </div>
-                            </div>
-                            <hr>
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <strong>Harga Fix:</strong><br>
-                                    ${data.fix_price ? 'Rp ' + new Intl.NumberFormat('id-ID').format(data.fix_price) : '-'}
-                                </div>
-                                <div class="col-md-4">
-                                    <strong>HET:</strong><br>
-                                    ${data.het_price ? 'Rp ' + new Intl.NumberFormat('id-ID').format(data.het_price) : '-'}
-                                </div>
-                                <div class="col-md-4">
-                                    <strong>Kemasan:</strong><br>
-                                    ${data.packaging || '-'}
-                                </div>
-                            </div>
-                        `;
-                        $('#kfaDetailContent').html(html);
-                    } else {
-                        $('#kfaDetailContent').html('<div class="alert alert-danger">' + (response.message || 'Gagal memuat detail produk.') + '</div>');
-                    }
-                },
-                error: function() {
-                    $('#kfaDetailContent').html('<div class="alert alert-danger">Terjadi kesalahan saat memuat data.</div>');
-                }
-            });
-        }
     </script>
 @endpush
