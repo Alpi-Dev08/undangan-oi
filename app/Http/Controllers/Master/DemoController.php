e317e5027fb6f88931a09edcac4b803d98eac1d1
Arif <arifmryn@gmail.com>
2026-04-02 02:23:04 +0700

    Fixed all fitur tema, add column template_file and template folder to template table.

--- app/Http/Controllers/Master/DemoController.php
+++ app/Http/Controllers/Master/DemoController.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Controllers\Master;
 
-use App\Models\Master\Template;
-use App\Models\Master\TemplateVideo;
 use App\Http\Controllers\Controller;
+use App\Models\Master\Template;
 
 class DemoController extends Controller
-{ 
+{   
     public function show($slug)
     {
-        $template = TemplateVideo::where('slug', $slug)
+        $template = Template::where('slug', $slug)
             ->where('status', 'aktif')
             ->firstOrFail();
 
-        return view('pages.demo.template_video', compact('template'));
+        // ambil folder dari database
+        $folder = $template->template_folder;
+
+        $path = public_path('templates/' . $folder . '/index.html');
+
+        // cek file ada atau tidak
+        if (!file_exists($path)) {
+            abort(404, 'Template tidak ditemukan.');
+        }
+
+        return redirect('templates/' . $folder . '/index.html');
     }
 }
